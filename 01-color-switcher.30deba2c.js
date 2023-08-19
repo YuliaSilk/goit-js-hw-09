dfe8a2cdc275156a4c103b2f4baff31362ddf955
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-08-19 10:17:26 +0000

    Deploying to gh-pages from @ YuliaSilk/goit-js-hw-09@d41ad97117b9574e070584eb3b3dc209ed75ba82 🚀

---
+++ 01-color-switcher.30deba2c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,timerId=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),d.addEventListener("click",(function(){d.disabled=!0,e.disabled=!1,clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.30deba2c.js.map
