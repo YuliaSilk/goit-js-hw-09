d41ad97117b9574e070584eb3b3dc209ed75ba82
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-08-19 13:17:06 +0300

    update 01-color-switchers.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+function onBtnStartClick() {
+   
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onBtnStopClick() {
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+    clearInterval(timerId);
+}
